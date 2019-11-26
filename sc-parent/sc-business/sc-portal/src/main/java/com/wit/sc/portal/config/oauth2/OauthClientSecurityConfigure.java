78582eaa2df2bc86e55fed17f06d4d8699f90bd5
曹庭旺 <caotingwang@gdhmail.com>
2019-11-26 17:03:38 +0800

    1、添加注释

--- sc-parent/sc-business/sc-portal/src/main/java/com/wit/sc/portal/config/oauth2/OauthClientSecurityConfigure.java
+++ sc-parent/sc-business/sc-portal/src/main/java/com/wit/sc/portal/config/oauth2/OauthClientSecurityConfigure.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * @author ctw
@@ -27,6 +28,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class OauthClientSecurityConfigure extends ResourceServerConfigurerAdapter {
 
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        super.configure(resources);
+    }
+
+    /**
+     * 用于配置对受保护的资源的访问规则
+     * 默认情况下所有不在/oauth/**下的资源都是受保护的资源
+     * @param http
+     * @throws Exception
+     */
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
